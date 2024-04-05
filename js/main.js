dbfa0809825df21e4bdf2b49633b5b41765cd8a5
Samandar <samandartursunov217@gmail.com>
2024-04-05 09:12:04 +0500

    Add carousel-product styles and functionaly

--- js/main.js
+++ js/main.js
@@ -37,9 +37,63 @@ function init () {
     body.classList.toggle("body-active");
 
   })
+  // CAROUSEL-PRODUCT
+  const imgItems = document.querySelectorAll(".carousel-product__img-item");
+  const imgPoints = document.querySelectorAll(".carousel-product__point");
+  const imgNextBtn = document.querySelector(".carousel-product__next");
+  const imgPrevBtn = document.querySelector(".carousel-product__prev");
+  let imgCount = 0;
+  
+  imgItems.forEach((item, index) => {
+    item.style.left = `${index * 100}%`;
+  })
 
+  imgPoints[imgCount].classList.add("carousel-product__point-active");
+  
+  function imgSlide() {
+    imgItems.forEach(item => {
+      item.style.transform = `translateX(-${imgCount * 100}%)`;
+    })
+    imgPoints.forEach(item => {
+      item.classList.remove("carousel-product__point-active");
+    })
+  }
 
+  function imgNext() {
+    if (imgCount < imgItems.length - 1) {
+      imgCount++
+      imgSlide()
+      imgPointActive()
+    } else {
+      imgCount = 0;
+      imgSlide()
+      imgPointActive()
+    }
+  }
+  function imgPrev() {
+    if (imgCount <= 0) {
+      imgCount = imgItems.length - 1;
+      imgSlide()
+      imgPointActive()
+    } else {
+      imgCount--
+      imgSlide()
+      imgPointActive()
+    }
+  }
+  function imgPointActive() {
+    imgPoints[imgCount].classList.add("carousel-product__point-active");
+  }
+  imgPoints.forEach((item, index) =>{
+    item.addEventListener("click", () => {
+      imgCount = index;
+      imgSlide()
+      imgPointActive()
+    })
+  })
 
+  imgNextBtn.addEventListener("click", imgNext);
+  imgPrevBtn.addEventListener("click", imgPrev);
 }
 
 document.addEventListener('DOMContentLoaded', init);
